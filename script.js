2d399ae947c01041742812eba22a31f03764e553
KevinYabut08 <Kevin.Yabut@edu.sait.ca>
2024-02-09 02:20:06 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let firstString = "this is our first string. yay!";
+let firstNumber = 42;
+
+const strElement = document.querySelector("#first-string");
+const numElement = document.querySelector("#first-number");
